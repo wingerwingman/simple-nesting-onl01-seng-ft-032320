d26acfa4b3c64b5d0217a0d0b664a7f4d1f17a18
Justin Winger <justinwinger4@gmail.com>
2020-03-26 21:02:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -79,9 +79,11 @@ def adding_matz
 
     
 programer_hash[:yukihiro_matsumoto] = {
-  known_for: ="Ruby",
-    languages: ["LISP", "C"]
-}
+  known_for:  "Ruby",
+    languages: 
+    ["LISP", "C"]
+  }
+
 end
 
 def changing_alan
