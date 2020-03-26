b8884a0589fe0a653268c6cc3a4e7d17ce71f16a
Justin Winger <justinwinger4@gmail.com>
2020-03-26 21:06:09 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,6 +106,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      program_hash[:alan_kay][:known_for] = "GUI"
+     program_hash
      
      
 end
